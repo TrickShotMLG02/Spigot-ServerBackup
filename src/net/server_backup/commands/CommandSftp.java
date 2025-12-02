ddac5a58ae1a6bb17cbac2af400ee9714c2cfa1c
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2025-12-02 19:43:56 +0100

    implemented CommandSftp

--- src/net/server_backup/commands/CommandSftp.java
+++ src/net/server_backup/commands/CommandSftp.java
@@ -1,11 +1,91 @@
 package net.server_backup.commands;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.server_backup.ServerBackup;
+import net.server_backup.core.OperationHandler;
+import net.server_backup.utils.SftpManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class CommandSftp {
 
     public static void execute(CommandSender sender, String[] args) {
-        // TODO: Implement
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (args[1].equalsIgnoreCase("list")) {
+            Bukkit.getScheduler().runTaskAsynchronously(ServerBackup.getInstance(), () -> {
+                SftpManager sftpm = new SftpManager(sender);
+
+                List<String> backups = sftpm.getSftpBackupList(false);
+
+                if (backups.size() == 0) {
+                    sender.sendMessage(OperationHandler.processMessage("Error.NoSftpBackups"));
+
+                    return;
+                }
+
+                try {
+                    int page = Integer.valueOf(args[2]);
+
+                    if (backups.size() < page * 10 - 9) {
+                        sender.sendMessage("Try a lower value.");
+
+                        return;
+                    }
+
+                    if (backups.size() <= page * 10 && backups.size() >= page * 10 - 10) {
+                        sender.sendMessage("----- Sftp-Backup " + Integer.valueOf(page * 10 - 9) + "-"
+                                + backups.size() + "/" + backups.size() + " -----");
+                    } else {
+                        sender.sendMessage("----- Sftp-Backup " + Integer.valueOf(page * 10 - 9) + "-"
+                                + Integer.valueOf(page * 10) + "/" + backups.size() + " -----");
+                    }
+                    sender.sendMessage("");
+
+                    for (int i = page * 10 - 10; i < backups.size() && i < page * 10; i++) {
+                        if (sender instanceof Player) {
+                            Player p = (Player) sender;
+
+                            TextComponent msg = new TextComponent(backups.get(i));
+                            msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                                    new ComponentBuilder("Click to get Backup name").create()));
+                            msg.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND,
+                                    backups.get(i).split(" ")[1]));
+
+                            p.spigot().sendMessage(msg);
+                        } else {
+                            sender.sendMessage(backups.get(i));
+                        }
+                    }
+
+                    int maxPages = backups.size() / 10;
+
+                    if (backups.size() % 10 != 0) {
+                        maxPages++;
+                    }
+
+                    sender.sendMessage("");
+                    sender.sendMessage("--------- Page " + page + "/" + maxPages + " ---------");
+                } catch (Exception e) {
+                    sender.sendMessage(OperationHandler.processMessage("Error.NotANumber").replaceAll("%input%", args[1]));
+                }
+            });
+        } else if (args[1].equalsIgnoreCase("download")) {
+            Bukkit.getScheduler().runTaskAsynchronously(ServerBackup.getInstance(), () -> {
+                SftpManager sftpm = new SftpManager(sender);
+
+                sftpm.downloadFileFromSftp(args[2]);
+            });
+        } else if (args[1].equalsIgnoreCase("upload")) {
+            Bukkit.getScheduler().runTaskAsynchronously(ServerBackup.getInstance(), () -> {
+                SftpManager sftpm = new SftpManager(sender);
+
+                sftpm.uploadFileToSftp(args[2], !ServerBackup.getInstance().getConfig().getBoolean("Sftp.CompressBeforeUpload"));
+            });
+        }
     }
 }
